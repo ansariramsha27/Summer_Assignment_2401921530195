1a43738bd2c3cd67e50766782089a46f9e6649c0
Ramsha Ansari <ansariramsha48@gmail.com>
2026-06-05 20:44:18 +0530

    Valid_Palindrome.cpp

---
+++ Week_1/Day_5/Valid_Palindrome.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isAlnum(char ch)
+    {
+        if((ch>='0'&&ch<='9')||(tolower(ch)>='a'&&tolower(ch)<='z'))
+        {
+            return true;
+        }
+        return false;
+    }
+    bool isPalindrome(string s) {
+        int start=0;
+        int end=s.length()-1;
+        while(start<end)
+        {
+            if(!isAlnum(s[start]))
+            {
+                start++;
+                continue;
+            }
+            if(!isAlnum(s[end]))
+            {
+                end--;
+                continue;
+            }
+            if(tolower(s[start])!=tolower(s[end]))
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
