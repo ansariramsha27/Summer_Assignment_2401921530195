f9008aebcfbb7743f7b78568ad72b4d6eb65466e
Ramsha Ansari <ansariramsha48@gmail.com>
2026-06-17 12:30:31 +0530

    Reverse_LinkedList.cpp

---
+++ Week_3/Day_2/Reverse_LinkedList.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev=nullptr;
+        ListNode *current=head;
+        while(current!=nullptr)
+        {
+            ListNode* temp=current->next;
+            current->next=prev;
+            prev=current;
+            current=temp;
+        }
+        return prev;
+    }
+};
