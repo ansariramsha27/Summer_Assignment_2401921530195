295df53cdaf0107742d694cd67d4ddf0511791d3
Ramsha Ansari <ansariramsha48@gmail.com>
2026-06-21 11:11:20 +0530

    Min_Stack.cpp

---
+++ Week_3/Day_3/Min_Stack.cpp
@@ -0,0 +1,31 @@
+class MinStack {
+public:
+    stack<int> s1,s2;
+    MinStack() {
+        
+    }
+    
+    void push(int value) {
+        if(s2.empty()||value<=s2.top())
+        {
+            s2.push(value);
+        }
+        s1.push(value);
+    }
+    
+    void pop() {
+        if(s1.top()==s2.top())
+        {
+            s2.pop();
+        }
+        s1.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+};
